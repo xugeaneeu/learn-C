e460d58ee3a959e36ceeb466972b90d9b8ff2d4a
Daniil Puchkov <d.puchkov@g.nsu.ru>
2023-10-23 12:09:57 +0700

    Hello World program, importance of \n

---
+++ let's start/helloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Hello World");
+    printf("Hello World");
+
+    printf("\n");
+
+    printf("Hello World\n");
+    printf("Hello World\n");
+
+    return 0;
+}
